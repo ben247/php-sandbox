2d22b840a27dd5cc527cdbad4b5c754e8f07a5b0
ben whalley <ben247@gmail.com>
2018-10-01 15:51:58 +0100

    Added chain gang folder

--- challenge_03.php
+++ challenge_03.php
@@ -1,21 +1,21 @@
 <?php
 
 class Bicycle {
-    puplic $brand;
-    puplic $model;
-    puplic $year;
-    puplic $description;
-    puplic $weight_kg;
+    public $brand;
+    public $model;
+    public $year;
+    public $description;
+    public $weight_kg;
 
-    function name() {
+    public function name() {
         return $this->brand . " " . $this->model . " (" . $this->year . ")";
     }
 
-    function weight_lbs() {
+    public function weight_lbs() {
         return floatval($this->weight_kg) * 2.2046226218;
     }
 
-    function set_weight_lbs($value) {
+    public function set_weight_lbs($value) {
         $this->weight_kg = floatval($value) / 2.2046226218;
     }
 }
