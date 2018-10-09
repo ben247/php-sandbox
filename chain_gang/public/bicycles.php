4c4226f83bc9298156f24b154c2e09c46bb90e66
ben whalley <ben247@gmail.com>
2018-10-09 14:44:39 +0100

    From instances to HTML

--- chain_gang/public/bicycles.php
+++ chain_gang/public/bicycles.php
@@ -28,22 +28,27 @@
 
       <?php
 
-      $args = ['brand' => 'Trek', 'model' => 'Emdonda', 'year' => '2017'];
+      $args = ['brand' => 'Trek', 'model' => 'Emdonda', 'year' => 2017, 'category' => 'Road', 'gender' => 'Unisex', 'colour' => 'Black', 'weight_kg' => 1.5, 'price' => 1000.00];
       $bike = new Bicycle($args);
 
       ?>
 
+      <!-- simple php loop -->
+      <?php for ($i = 0; $i <= 0; $i++) { ?>
+      <!-- h is to escape html for security -->
       <tr>
         <td><?php echo h($bike->brand); ?></td>
         <td><?php echo h($bike->model); ?></td>
         <td><?php echo h($bike->year); ?></td>
         <td><?php echo h($bike->category); ?></td>
         <td><?php echo h($bike->gender); ?></td>
-        <td>Color</td>
-        <td>Weight</td>
-        <td>Condition</td>
-        <td>Price</td>
+        <td><?php echo h($bike->colour); ?></td>
+        <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
+        <td><?php echo h($bike->condition()); ?></td>
+        <td><?php echo h(number_format('$', $bike->price)); ?></td>
       </tr>
+<?php 
+} ?>
 
     </table>
   </div>
